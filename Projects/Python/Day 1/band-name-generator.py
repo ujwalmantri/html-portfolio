7f26c44a5bbf4dc470679b39f7e497a62d48d855
Ujwal Mantri <ujwalmantrifr@gmail.com>
2025-10-04 20:21:54 +0530

    idk tf is going on but okay

--- Projects/Python/Day 1/band-name-generator.py
+++ Projects/Python/Day 1/band-name-generator.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 print("Welcome to Band Name Generator!")
 city_name = input("What's the name of the city you grew up in?\n").upper()
 pet_name = input("What's your pet's name?\n").capitalize()
-=======
-print("Welcome to Band Name Generator!")
-city_name = input("What's the name of the city you grew up in?\n").upper()
-pet_name = input("What's your pet's name?\n").capitalize()
->>>>>>> a9bc62e36c5ec236c2ca38a046bf31e6759214bc
 print("Your band name could be " + city_name + " " + pet_name)
--- Projects/Python/Day 3/treasure-hunt.py
+++ Projects/Python/Day 3/treasure-hunt.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print(r'''
  _                        
                       \`*-.                    
@@ -42,49 +41,4 @@ if left_or_right == "left":
     else:
         print("Attacked by trout. Game Over!")
 else:
-=======
-print(r'''
- _                        
-                      \`*-.                    
-                       )  _`-.                 
-                      .  : `. .                
-                      : _   '  \               
-                      ; *` _.   `*-._          
-                      `-.-'          `-.       
-                        ;       `       `.     
-                        :.       .        \    
-                        . \  .   :   .-'   .   
-                        '  `+.;  ;  '      :   
-                        :  '  |    ;       ;-. 
-                        ; '   : :`-:     _.`* ;
-               [bug] .*' /  .*' ; .*`- +'  `*' 
-                     `*-*   `*-*  `*-*'        
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
-print("You're at a cross road. Where do you want to go?")
-
-left_or_right = input("Type 'right' or 'left'\n").lower()
-if left_or_right == "left":
-    print("You've come to a lake. "
-          "There is an island in the middle of the lake.")
-    swim_or_not=input("Type 'wait' to wait for a boat. "
-                      "Type 'swim' to swim across.\n").lower()
-    if swim_or_not == "wait":
-        print("You arrive at the island unharmed. "
-              "There is a house with 3 doors.")
-        which_color=input("One red, one yellow and one blue. "
-                          "Which one do you choose?\n").lower()
-        if which_color == "yellow":
-            print("You found the treasure! You Win!")
-        elif which_color == "red":
-            print("It's a room full of fire. Game Over!")
-        elif which_color == "blue":
-            print("You enter a room of beasts. Game Over!")
-        else:
-            print("You chose the room that doesn't exist. Game Over!")
-    else:
-        print("Attacked by trout. Game Over!")
-else:
->>>>>>> a9bc62e36c5ec236c2ca38a046bf31e6759214bc
     print("You fall into a river. Game Over!")
